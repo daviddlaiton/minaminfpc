21394007c9f842349f3c9f281951441507cb1b35
Andrés David Laiton Vargas <ad.laiton10@uniandes.edu.co>
2018-05-21 18:12:52 -0500

    Comentarios sobre información del idioma
    Comentarios sobre información del idioma en el que la página se encuentra.

--- server/main.js
+++ server/main.js
@@ -3,4 +3,5 @@ import '../imports/api/methods';
 
 Meteor.startup(() => {
   // code to run on server at startup
+  //Aquí pudieron haber incluido un tag para que informe el idioma en el que la página se encuentra.
 });
